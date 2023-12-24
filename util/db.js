33f5033530c2e8bb2efb7961aaaf641d1bd6b921
Gunjan Kumar <gunjanraj321@gmail.com>
2023-12-24 20:03:02 +0530

    changes

--- util/db.js
+++ util/db.js
@@ -7,10 +7,10 @@ try {
 const mysql = require('mysql2');
 const { Sequelize } = require('sequelize');
 
-console.log(process.env.DB_NAME);
-console.log(process.env.DB_USER);
-console.log(process.env.DB_PASSWORD);
-console.log(process.env.DB_HOST)
+// console.log(process.env.DB_NAME);
+// console.log(process.env.DB_USER);
+// console.log(process.env.DB_PASSWORD);
+// console.log(process.env.DB_HOST)
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
     host: process.env.DB_HOST,
     // port: 3306,
